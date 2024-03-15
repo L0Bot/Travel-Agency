bfc0cec657b85fc6896adc6745e95927bbb72a9f
L0Bot <botterilouis@gmail.com>
2024-03-15 12:10:34 +0100

    navbar component creation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+  function isItClicked() {
+    if (!click) {
+      return "menu-icon fas fa-bars";
+    } else {
+      return "menu-icon fas fa-times";
+    }
+  }
+  function ImIActivated() {
+    if (!click) {
+      return "nav-menu";
+    } else {
+      return "nav-menu active";
+    }
+  }
+  function handleClick() {
+    setClick(!click);
+  }
+  function closeMobileMenu() {
+    setClick(false);
+  }
   return (
-    <div>
-      
-    </div>
-  )
+    <nav className="navbar">
+      <Link to="/" className="navbar-logo">
+        <i class="fa-solid fa-location-dot"></i>TRVL
+      </Link>
+      <i className={isItClicked()} onClick={handleClick}></i>
+      <ul className={ImIActivated()}>
+        <li className="nav-item">
+          <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
+            Services
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/products" className="nav-links" onClick={closeMobileMenu}>
+            Products
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
+            Sign up
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
+export default Navbar;
