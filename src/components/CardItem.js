1914ee62755dfb722b10e49267937a7ee733deef
L0Bot <botterilouis@gmail.com>
2024-03-15 22:43:20 +0100

    cardz avancement

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function CardItem() {
+function CardItem(props) {
   return (
    <article className="cards__item">
-    <Link className="cards__item__link">
-      <figure className="cards__item__pic-wrap">
-        <img src="" alt="Travel" className="cards__item__img" />
+    <Link className="cards__item__link" to={props.path}>
+      <figure className="cards__item__pic-wrap" data-category={props.label}>
+        <img src={props.src} alt="Travel" className="cards__item__img" />
       </figure>
       <div className="cards__item__info">
-        <h5 className="cards__item__text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa vel quos assumenda?</h5>
+        <h5 className="cards__item__text">{props.text}</h5>
       </div>
     </Link>
    </article>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,20 +1,61 @@
-import React from 'react'
-import CardItem from './CardItem'
+import React from "react";
+import CardItem from "./CardItem";
+import "./Cards.css";
 
 function Cards() {
   return (
-    <section className='cards'>
+    <section className="cards">
       <h1>Guettes moi ça un peu !</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
           <div className="cards__items">
-            <CardItem />
+            <CardItem
+              src="images/barquesmontagne.jpg"
+              text="Explorez les lacs et rivières en profondeur. Un séjour en pleine nature qui ravira le baroudeur en vous."
+              label="Aventure"
+              path="/services"
+            />
+            <CardItem
+              src="images/ilepiscine.jpg"
+              text="Profitez pleinement d'un repos emplement merité. Les destinations de vos rêves vous attendent !"
+              label="Paix intérieure"
+              path="/services"
+            />
+            <CardItem
+              src="images/italiefalaiseville.jpg"
+              text="Découvrez l'italie sous toutes ses couleurs. Dépaysement garanti"
+              label="Découverte"
+              path="/services"
+            />
+            <CardItem
+              src="images/mongolfiere.jpg"
+              text="Laissez-vous transporter par la magie des montgolfières et vivez des moments inoubliables dans les airs."
+              label="Découverte"
+              path="/services"
+            />
+            <CardItem
+              src="images/vandesert.jpg"
+              text="Traversez les déserts majestueux. Vivez des expériences authentiques au rythme de votre propre aventure nomade."
+              label="Aventure"
+              path="/services"
+            />
+            <CardItem
+              src="images/venisepont.jpg"
+              text="Découvrez Venise, la cité emblématique, où romance et histoire se rencontrent le long de ses canaux."
+              label="Instants Inoubliables"
+              path="/services"
+            />
+            <CardItem
+              src="images/vanfalaise.jpg"
+              text="Découvrez des paysages époustouflants, des nuits étoilées inoubliables et des expériences uniques au bord des falaises."
+              label="Aventure"
+              path="/services"
+            />
           </div>
         </div>
       </div>
-
     </section>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
